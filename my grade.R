83087a33b9aa324e7f9bb1c93a9361030acc05f0
orcunTasdemir <orcunTasdemir@users.noreply.github.com>
2019-12-16 01:53:38 -0500

    finalfinal

---
+++ my grade.R
@@ -0,0 +1,5 @@
+# I think my grade should be a 3 as I showed sufficient effort in my first try
+# and commented out everything in my final submission and did all the necessary
+# editting and corrections
+# 
+# my grade:  3
