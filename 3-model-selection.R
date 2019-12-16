d53669fc536d42d5fbc4ae3ac0e8bdb82a529778
orcunTasdemir <orcunTasdemir@users.noreply.github.com>
2019-12-16 01:50:54 -0500

    final

--- 3-model-selection.R
+++ 3-model-selection.R
@@ -58,6 +58,8 @@ sum(test2$correct) / length(test2$correct)
 # Can both models do a reasonable job of accounting for the mean RT and accuracy? Report the
 # results of your efforts:
 
+#Initially I thought that the model was not complex enough to give the correct accuracy level
+
 ## I believe they both do reasonably well in predicting the RT means but I don't know if the
 ## same can be said for the accuracy values. Trying to fit the data for the mean RT rates was
 ## easier, however, I could not do it for the accuracy levels for either of the models. Accuracy
@@ -71,13 +73,63 @@ sum(test2$correct) / length(test2$correct)
 # Describe briefly how you might make this evaluation.
 
 
-hist(test$rt)
-hist(test2$rt)
+# hist(test$rt)
+# hist(test2$rt)
+
+#initially I thought I only needed to plot these two histograms above
+#seeing your solutions, ı am now plotting the new histograms too.
+
+layout(matrix(1:4, nrow=2, byrow=T))
+
+hist((test2 %>% filter(correct==TRUE))$rt, breaks=seq(0,2000,100), main="RW Model, correct", xlab="RT")
+hist((test2 %>% filter(correct==FALSE))$rt, breaks=seq(0,2000,100), main="RW Model, incorrect", xlab="RT")
+
+#I found using a range from 0 to 500 visually more understandable for the histogram for the accumulator model (ACC)
+hist((test %>% filter(correct==TRUE))$rt, breaks=seq(0,500,10), main="ACC Model, correct", xlab="RT")
+hist((test %>% filter(correct==FALSE))$rt, breaks=seq(0,500,10), main="ACC Model, incorrect", xlab="RT")
+
+##My initial attempt to answer the question is below where it looks like I am trying to say that
+##the random walk model gives us more scattered values for Rt which means that it is not
+##very precise. The variability, which is something you also talked about in your solution.
+##However, apart from the precision, what I needed to comment on is the fact that the standard deviation
+## we get from the two models are significantly different as you pointed out.
+##When we calculate the standard deviations for each model:
+
+#random walk model is test2
+test2 %>% group_by(correct) %>% summarize(sd.rt = sd(rt))
+# correct sd.rt
+# <lgl>   <dbl>
+# FALSE    171.
+# TRUE     188.
+
+#accumulator model is test
+test%>% group_by(correct) %>% summarize(sd.rt = sd(rt))
+# correct sd.rt
+# <lgl>   <dbl>
+# FALSE    12.2
+# TRUE     14.8
+
+#As we see the standard deviation the random walk model generates is very high
+#compared to the standard deviation from the accumulator model, which means that
+#for these accuracy and rt levels for both correct and incorrect responses, accumulator
+#model is much better at fitting the data.
+
+
+
+#Another thing you said about the accumulator model's rt level graphs being more symmetric
+#also makes sense in that the AC model does not have an unnatural favoring of response times that are
+#shorter as the RW model does, instead the response times around the mean response time are
+#evenly distributed in terms of their frequency of occurence.
+
+
+#BELOW is my initial commentary which I think still communicates the ideas
+#in an informal way but given the standard deviation calculations now,
+#It is more grounded. The part about the MDD does not make sense to me now, or rather that I am not sure if makes sense.
 
 ## I think the accumulator model is a better way to plot for this data.
 ## The reason is that the random-walk model has a wider range that has
 ## the majority of the total frequency value spread widely over the graph which
-##O creates a sense of possible inprecision and ambiguity for the model's part.
+## creates a sense of possible inprecision and ambiguity for the model's part.
 ## The accumulator model on the other hand is much less spread and produces a better
 ## difference between the frequent occurences of the RT means and the much less
 ## infrequent values. It has a spike at one value another that is very close to the
